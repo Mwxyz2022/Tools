95685e943dee6ce3f1b0b917ff343742dd2a6a34
Mwxyz2022 <rozhukvlad@gmail.com>
2022-12-14 23:15:56 +0200

    fixed files

--- lesson3/task1/dist/index.js
+++ lesson3/task1/dist/index.js
@@ -1,10 +1,8 @@
-"use strict";
-
-require("core-js/modules/es.array.flat.js");
-require("core-js/modules/es.array.unscopables.flat.js");
-require("core-js/modules/es.object.to-string.js");
-require("core-js/modules/es.promise.js");
-require("core-js/modules/es.promise.finally.js");
+import "core-js/modules/es.array.flat.js";
+import "core-js/modules/es.array.unscopables.flat.js";
+import "core-js/modules/es.object.to-string.js";
+import "core-js/modules/es.promise.js";
+import "core-js/modules/es.promise.finally.js";
 var data = [1, 2, [3, 4, [5, 6]]];
 var result = data.flat(2);
 console.log(result);
