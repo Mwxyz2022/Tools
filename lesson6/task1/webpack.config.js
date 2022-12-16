6757ec105f2904a3345d730381e44a80ea488c97
Mwxyz2022 <rozhukvlad@gmail.com>
2022-12-16 22:37:30 +0200

    fixed

--- lesson6/task1/webpack.config.js
+++ lesson6/task1/webpack.config.js
@@ -16,6 +16,8 @@ module.exports = {
             loader: 'url-loader',
             options: {
               limit: 8192,
+              name: '[name].[ext]',
+              outputPath: 'images',
             },
           },
         ],
